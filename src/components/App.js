73bacceb131efe228de0719f48f5a90163f4716c
devwqc <devwqc@gmail.com>
2024-02-07 16:47:43 +0900

    feat: && 연산자를 활용한 버튼 숨기기

--- src/components/App.js
+++ src/components/App.js
@@ -45,9 +45,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button disabled={!hasNext} onClick={handleLoadMore}>
-        더보기
-      </button>
+      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
