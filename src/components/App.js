fb3beb0a0111e3101860eedb5ae898d19dcc23f7
devwqc <devwqc@gmail.com>
2024-02-07 19:42:20 +0900

    feat: isLoading state를 활용한 네트워크 로딩 처리

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ function App() {
   const [order, setOrder] = useState('createdAt');
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
@@ -20,7 +21,16 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
@@ -45,7 +55,9 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
+      <button disabled={isLoading} onClick={handleLoadMore}>
+        더보기
+      </button>
     </div>
   );
 }
