c6723500bafe64c1313959160821f5f954f71047
devwqc <devwqc@gmail.com>
2024-02-07 10:25:28 +0900

    feat: 영화 리스트를 mock.json에서 fetch함수로 받아오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import mockItems from '../mock.json';
 import ReviewList from './ReviewList';
+import { getReviews } from '../api';
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState('createdAt');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -15,6 +15,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -22,6 +27,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
