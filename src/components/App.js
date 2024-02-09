24e866ad545a8997392c37f882ecefd6b18e10c1
devwqc <devwqc@gmail.com>
2024-02-09 10:37:31 +0900

    feat: response로 넘어온 생성한 리뷰로 setItems 반영

--- src/components/App.js
+++ src/components/App.js
@@ -46,6 +46,10 @@ function App() {
     handleLoad({ order, offset, limit: LIMIT });
   };
 
+  const handleSubmitSuccess = (review) => {
+    setItems((preItems) => [review, ...preItems]);
+  };
+
   useEffect(() => {
     handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
@@ -56,7 +60,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
-      <ReviewForm />
+      <ReviewForm onSubmitSuccess={handleSubmitSuccess} />
       <ReviewList items={sortedItems} onDelete={handleDelete} />
       <button disabled={isLoading} onClick={handleLoadMore}>
         더보기
